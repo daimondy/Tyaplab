96b496de5b451902273f88d9b95edd0d84cfe295
Elchi DIana <dianaelchi@gmail.com>
2019-03-16 16:31:26 +0300

    a

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistationForm, LoginForm
 
 app = Flask(__name__)
-
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}'), '{self.image_file}'"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+
 posts = [
     {
-        'message':'Добро пожаловать на мой сайт. Здесь будет висеть рассписание группы БСТ1803'
+        'message': 'Добро пожаловать на мой сайт. Здесь будет висеть рассписание группы БСТ1803'
     }
 ]
 
@@ -16,22 +35,24 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts = posts, title = 'Главная страница')
+    return render_template('home.html', posts=posts, title='Главная страница')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'О сайте')
+    return render_template('about.html', title='О сайте')
 
-@app.route('/register', methods = ['GET', 'POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistationForm()
     if form.validate_on_submit():
         flash(f'Аккаунт создан с именем {form.username.data}.', 'success')
-        return  redirect(url_for('home'))
-    return render_template('register.html', title='Регистрация', form = form)
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Регистрация', form=form)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -40,7 +61,7 @@ def login():
             return redirect(url_for('home'))
         else:
             flash('Авторизация не прошла успешно. Пожалуйста, проверьте свой логин и пароль', 'danger')
-    return render_template('login.html', title='Вход', form = form)
+    return render_template('login.html', title='Вход', form=form)
 
 
 if __name__ == '__main__':
