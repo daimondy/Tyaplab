ebd6d9132a0fc755781cd796ba9efdde09b0081c
Elchi DIana <dianaelchi@gmail.com>
2019-03-16 23:52:26 +0300

    Почти готово

--- server/forms.py
+++ server/forms.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from server.models import User
 
 class RegistationForm(FlaskForm):
     username = StringField('Имя пользователя',
@@ -13,9 +14,18 @@ class RegistationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Регистрация')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Это имя уже занято. Пожалуйста, выберите другое.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email уже занят. Пожалуйста, выберите другое.')
+
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember = BooleanField('Запомнить пароль')
     submit = SubmitField('Вход')
--- server/routes.py
+++ server/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, url_for, flash, redirect
-
-from server import app
+from flask import render_template, url_for, flash, redirect, request
+from server import app, db, bcrypt
 from server.forms import RegistationForm, LoginForm
+from server.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
-        'message': 'Добро пожаловать на мой сайт. Здесь будет висеть рассписание группы БСТ1803'
+        'message': 'Добро пожаловать на мой сайт. Здесь будет висеть рассписание группы БСТ1803. '
+                   'Для того, чтобы увидеть рассписание, войдите в свой профиль.'
     }
 ]
 
@@ -23,20 +25,42 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistationForm()
     if form.validate_on_submit():
-        flash(f'Аккаунт создан с именем {form.username.data}.', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Ваш аккаунт создан! Теперь вы можете войти', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('Вы вошли в систему!')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(f'Авторизация не прошла успешно. Пожалуйста, проверьте свой логин и пароль', 'danger')
+            flash('Авторизация не прошла успешно. Пожалуйста, проверьте свой логин и пароль', 'danger')
     return render_template('login.html', title='Вход', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
